6aabf781605894d208073577f9398b8f66943c9c
cartman1978 <cartman_7802@yahoo.ie>
2021-02-28 10:45:27 +0000

    added question in game.js

--- game.js
+++ game.js
@@ -9,3 +9,38 @@ let acceptingAnswer =true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'What is 5 x 15?',
+        choice1: '75',
+        choice2: '85',
+        choice3: '65',
+        choice4: '105',
+        answer: 1,
+    },
+     {
+        question: 'Which is the biggest Country in the world?',
+        choice1: 'China',
+        choice2: 'Russia',
+        choice3: 'Canada',
+        choice4: 'USA',
+        answer: 2,
+    },
+     {
+        question: 'Which is the fastest car in the world?',
+        choice1: 'SSC Tuatara',
+        choice2: 'Bugatti Venom',
+        choice3: 'Ferrari Enzo',
+        choice4: 'Koenigsegg Agera',
+        answer: 1,
+    },
+     {
+        question: 'The Tallest building in the world is located in which city?',
+        choice1: 'Dubai',
+        choice2: 'New York',
+        choice3: 'Shanghai',
+        choice4: 'None of the above',
+        answer: 1,
+    }
+]
